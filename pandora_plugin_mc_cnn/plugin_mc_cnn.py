1d9dc56c7456d14f304d18575cd15c36755c6103
Veronique Defonte <veronique.defonte@c-s.fr>
2021-06-16 18:10:53 +0200

    style: add copyright header

--- pandora_plugin_mc_cnn/plugin_mc_cnn.py
+++ pandora_plugin_mc_cnn/plugin_mc_cnn.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright: (c) 2019 Centre National d'Etudes Spatiales
-
+# coding: utf8
+#
+# Copyright (c) 2021 Centre National d'Etudes Spatiales (CNES).
+#
+# This file is part of PANDORA
+#
+#     https://github.com/CNES/Pandora_plugin_mccnn
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 """
 This module contains all functions to calculate the cost volume with mc-cnn networks
 """
@@ -120,12 +137,12 @@ class MCCNN(matching_cost.AbstractMatchingCost):
         if offset_row_col != 0:
             # Fast architecture
             if self._mc_cnn_arch == 'fast':
-                cv[offset_row_col: -offset_row_col, offset_row_col:-offset_row_col, :] = run_mc_cnn_fast(img_left,
-                                                                    img_right, disp_min, disp_max, self._model_path)
+                cv[offset_row_col: -offset_row_col, offset_row_col:-offset_row_col, :] = run_mc_cnn_fast(
+                    img_left, img_right, disp_min, disp_max, self._model_path)
             # Accurate architecture
             else:
-                cv[offset_row_col: -offset_row_col, offset_row_col:-offset_row_col, :] = run_mc_cnn_accurate(img_left,
-                                                                    img_right, disp_min, disp_max, self._model_path)
+                cv[offset_row_col: -offset_row_col, offset_row_col:-offset_row_col, :] = run_mc_cnn_accurate(
+                    img_left, img_right, disp_min, disp_max, self._model_path)
         else:
             # Fast architecture
             if self._mc_cnn_arch == 'fast':
