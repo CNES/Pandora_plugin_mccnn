9b2b0fb695cc7d89305ea5fb243ba96b22ec9e0b
seillaj <julien.seillade@csgroup.eu>
2025-10-30 16:09:09 +0000

    feat: removed torch dependancy to cpp functions

--- pandora_plugin_mc_cnn/plugin_mc_cnn.py
+++ pandora_plugin_mc_cnn/plugin_mc_cnn.py
@@ -181,7 +181,7 @@ class MCCNN(matching_cost.AbstractMatchingCost):
         schema["window_size"] = And(int, lambda x: x == 11)
         schema["model_path"] = And(str, lambda x: os.path.exists(x))
         schema["framework"] = And(str, lambda x: x in ["pytorch", "onnx", "openvino"])
-        schema["variant"] = And(str, lambda x: x in ["baseline", "opt1", "opt2", "cpp", "cpp2"])
+        schema["variant"] = And(str, lambda x: x in ["baseline", "opt1", "opt2", "cpp", "cpp2", "cpp_notorch", "cpp2_notorch"])
         schema["provider"] = And(str, lambda x: x in ["cpu_base", "openvino"])
         if "model_name" in cfg:
             schema["model_name"] = str
